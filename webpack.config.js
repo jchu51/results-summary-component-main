bfa11ffd4485578b9bc02f0f3c5c66c964cd363a
Jonathan Chueh <jonahtanchueh1992@gmail.com>
2023-02-19 23:24:52 +1100

    install styled-components, svgr/webpack, file-loader, url-loader and update webpack config and ts config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const prod = process.env.NODE_ENV === "production";
-
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
   mode: prod ? "production" : "development",
@@ -18,9 +19,33 @@ module.exports = {
         },
         use: "ts-loader",
       },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|png)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192, // Convert files < 8kb to base64 strings
+              fallback: "file-loader", // Fallback to file-loader for larger files
+              name: "[name].[ext]", // Name of the output file
+              outputPath: "assets/fonts", // Output directory for fonts
+            },
+          },
+        ],
+      },
     ],
   },
   devtool: prod ? undefined : "source-map",
+  devServer: {
+    static: path.resolve(__dirname, "public"),
+    compress: true,
+    port: 3000,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
